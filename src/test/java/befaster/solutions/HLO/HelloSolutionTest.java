f1ad55b65c8c7757f6fb96cc5dac57f0a97afc05
sbx_user1051 <sbx_user1051@169.254.43.45>
2024-02-16 12:37:10 +0000

    Fri Feb 16 12:37:10 UTC 2024

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -1,2 +1,24 @@
-package befaster.solutions.HLO;public class HelloSolutionTest {
+package befaster.solutions.HLO;
+
+import befaster.solutions.SUM.SumSolution;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class HelloSolutionTest {
+
+    private HelloSolution hello;
+
+    @BeforeEach
+    public void setUp() {
+        hello = new HelloSolution();
+    }
+
+    @Test
+    public void print_hello() {
+        assertThat(hello.hello("Frank"), equalTo("Hello, Frank!"));
+    }
 }
+
