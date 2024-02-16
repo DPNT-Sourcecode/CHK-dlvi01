5ade6da604d52b7243cf922194beed06290b7772
sbx_user1051 <sbx_user1051@169.254.43.45>
2024-02-16 13:10:03 +0000

    Fri Feb 16 13:10:03 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,11 +1,81 @@
 package befaster.solutions.CHK;
 
-import befaster.runner.SolutionNotImplementedException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
+        // This variable represents the total checkout value of the items.
+        int checkoutSum = 0;
+
+        // This map stores individual unit prices (no special offers).
+        Map<String, Integer> unitPrices = new HashMap<>();
+        unitPrices.put("A", 50);
+        unitPrices.put("B", 30);
+        unitPrices.put("C", 20);
+        unitPrices.put("D", 15);
+
+        // Store SKUs in a HashMap, so we can identify if there are multiples of 3As, or 2Bs, for example.
+        Map<String, Integer> skuCounts = getSkuCounts(skus);
+
+        // Calculate the checkout sum.
+        for (Map.Entry<String, Integer> entry : skuCounts.entrySet()) {
+            String sku = entry.getKey();
+            int count = entry.getValue();
+
+            if (sku == "A") {
+                int specialOfferUnitRequirement = 3;
+                int specialOfferPrice = 130;
+                checkoutSum += handleSpecialOffer(unitPrices, sku, count,  specialOfferUnitRequirement, specialOfferPrice);
+            } else if (sku == "B") {
+                int specialOfferUnitRequirement = 2;
+                int specialOfferPrice = 45;
+                checkoutSum += handleSpecialOffer(unitPrices, sku, count,  specialOfferUnitRequirement, specialOfferPrice);
+            } else {
+                checkoutSum += count * unitPrices.get(sku);
+            }
+
+        }
+
+        return checkoutSum;
+    }
+
+    private static Map<String, Integer> getSkuCounts(String skus) {
+        Map<String, Integer> skuCounts = new HashMap<>();
+
+        // Split the string around whitespaces.
+        // Assuming SKUs are separated by whitespace.
+        String[] skuArray = skus.split(" ");
+
+        // Store all individual SKUs and their counts.
+        for (String sku : skuArray) {
+            if (!skuCounts.containsKey(sku)) {
+                skuCounts.put(sku, 1);
+            } else {
+                int currentCount = skuCounts.get(sku);
+                currentCount++;
+                skuCounts.put(sku, currentCount);
+            }
+        }
+        return skuCounts;
+    }
+
+    private static int handleSpecialOffer(Map<String, Integer> unitPrices, String sku, int count, int specialOfferUnitRequirement, int specialOfferPrice) {
+        // Handle special offers.
+
         int sum = 0;
+
+        int specialOfferCount = count / specialOfferUnitRequirement;
+
+        if (specialOfferCount > 0) {
+            sum += specialOfferCount * specialOfferPrice;
+        }
+
+        // Handle if there are any extra items that are not covered bt the special offer, say a 4th A item.
+        count = count - specialOfferCount * specialOfferUnitRequirement;
+        sum += count * unitPrices.get(sku);
 
         return sum;
     }
 }
+
--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -24,5 +24,28 @@ public class CheckoutSolutionTest {
         assertThat(checkout.checkout(items), equalTo(expectedSum));
     }
 
+    @Test
+    public void test_checkout_with_multiple_items() {
+        String items = "A B";
+        int expectedSum = 80;
 
+        assertThat(checkout.checkout(items), equalTo(expectedSum));
+    }
+    
+    @Test
+    public void test_checkout_with_special_offers() {
+        String items = "A A A";
+        int expectedSum = 130;
+
+        assertThat(checkout.checkout(items), equalTo(expectedSum));
+    }
+
+    @Test
+    public void test_checkout_with_special_offers_and_multiple_items() {
+        String items = "A A A C D";
+        int expectedSum = 165;
+
+        assertThat(checkout.checkout(items), equalTo(expectedSum));
+    }
 }
+
